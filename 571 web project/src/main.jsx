25ca7aeda31abc12cb17ea9891bc732d77929eb7
Ryan Porter <rmporter2@wisc.edu>
2026-03-17 17:13:24 -0500

    deployment with HashRouter

--- 571 web project/src/main.jsx
+++ 571 web project/src/main.jsx
@@ -4,7 +4,10 @@ import './index.css'
 import App from './App.jsx'
 
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { HashRouter } from 'react-router-dom';
 
 createRoot(document.getElementById('root')).render(
-    <App />
+    <HashRouter>
+        <App />
+    </HashRouter>
 )

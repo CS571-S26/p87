493e323713dadd056ceeccd7d72c2ca0cd085f2f
Ryan Porter <rmporter2@wisc.edu>
2026-04-29 13:11:17 -0500

    personal homepage

--- src/nav/pages/Home.jsx
+++ src/nav/pages/Home.jsx
@@ -1,23 +1,46 @@
 import React from "react";
 import wtb136 from '../../assets/WTB_136.png';
+import teddyTheGoat from "../../assets/teddyTheGoat.png"
+import definitelyMadison from "../../assets/definitelyMadison.png"
 
 const Home = () => {
 
     return (
     <div style={{ padding: '20px', fontFamily: 'sans-serif' }}>
-      <header>
-        <p>Placeholder homepage. Welcome to the UW-Madison Smash Brother's club!</p>
+      <header style={{backgroundColor: "red"}}>
+        <h1>Welcome to the UW-Madison Smash Club Website!</h1>
       </header>
 
       <main style={{ marginTop: '40px' }}>
         <section>
-          <h2>Getting Started</h2>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-             Sed sit amet viverra dui. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-              Duis blandit vulputate ipsum, vitae rutrum augue hendrerit eu.
-               In sit amet nulla faucibus, accumsan turpis porta, laoreet quam. Praesent eu scelerisque velit. 
-               Cras posuere varius posuere. Pellentesque id augue eu nulla efficitur maximus at in lectus. 
-               Maecenas nec orci sed felis interdum elementum.
+          <h2>About Us</h2>
+          <p>The Madison Smash Club is an easygoing, informal club that (you guessed it) plays Super Smash Bros. There is
+            no club fee, and anyone can come to as many or as few club events as they like. We have weekly meetings ("smashfests")
+            every Thursday in Ingraham Hall, as well as biweekly and monthly tournaments. Our main goals are to have fun, foster community,
+            and provide a space for casual and competitve players alike to share their love of the game.
+           </p>
+
+          <br></br>
+          <img src={teddyTheGoat} alt="several members of the smash club at a tournament"/>
+          <br></br>
+          <br></br>
+
+           <h2>How Do I Join?</h2>
+           <p>Just show up to an event! You don't have to officially join the club in any way. We do however have a club Discord sever
+            where you can get information about upcoming events, chat with other members, and learn about the community. Be sure to check out
+            our "Events" page to see our upcoming meetings, and visit our "Resources" page if you'd like to join the Discord server!
+           </p>
+
+          <br></br>
+          <img src={definitelyMadison} alt="players competing at a regional tournament"/>
+          <br></br>
+          <br></br>
+
+           <h2>What Can I Do On This Website?</h2>
+           <p>So far, this website is mostly informational. We'll have info on upcoming club events, how to find and compete in local
+            tournaments, and some other fun features like a personalizable "Bios" page. If you're looking to get into the action right away, we 
+            highly recommend joining the Discord and seeing if anyone wants to play!
+
            </p>
            <img src={wtb136} alt="A results graphic from a Smash Brothers event, humerously drawn in crude MS-paint."
             width = '800' height = '600'/>
